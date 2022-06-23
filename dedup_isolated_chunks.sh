fce0541aa0f927d915e0fb572f8cfb8f584bb448
Joey Öhman <joeyoh@kth.se>
2022-06-23 11:54:06 +0200

    prep isolated chunk dedup

--- dedup_isolated_chunks.sh
+++ dedup_isolated_chunks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-COMMON_PATH="/data/nordic_pile/jsonl_train_format/exact_dedup/en"
+COMMON_PATH="/data/nordic_pile/jsonl_train_format"
 
 ROOT_IN="$COMMON_PATH/chunked_before_fuzzy"
 ROOT_OUT="$COMMON_PATH/fuzzy_dedup"
 
-chunk_dirs=("chunk1" "chunk2" "chunk3" "chunk4" "chunk5" "chunk6" "chunk7" "chunk8" "chunk9" "chunk10" "chunk11")
+chunk_dirs=("chunk1" "chunk2" "chunk3" "chunk4" "chunk5" "chunk6" "chunk7" "chunk8" "chunk9")
 
 for chunk_dir in ${chunk_dirs[@]}; do
   # Create full input path
@@ -17,6 +17,6 @@ for chunk_dir in ${chunk_dirs[@]}; do
   echo "Deduplicating single directory with isolated chunk."
   echo "In dir: $chunk_path_in"
   echo "Out dir: $chunk_path_out"
-  # bash dedup_single_dir.sh $chunk_path_in $chunk_path_out
+  bash dedup_single_dir.sh $chunk_path_in $chunk_path_out
 done
 
